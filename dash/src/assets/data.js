789e0335bdb06e33bb4fdaff98db50ab793a2e5f
AndrewGlago <andrewglago1@gmail.com>
2021-09-29 16:49:19 +0000

    placeholder data

---
+++ dash/src/assets/data.js
@@ -0,0 +1,249 @@
+// import faker from "faker";
+// const faker = require("faker");
+
+// const testData = [];
+
+// String.prototype.capitalize = function () {
+//   return this.charAt(0).toUpperCase() + this.slice(1);
+// };
+
+// for (let i = 0; i < 15; i++) {
+//   const arr = {
+//     pod: `${String(faker.random.word()).capitalize()} Room`,
+//     users: [],
+//   };
+
+//   for (let a = 0; a < 10; a++) {
+//     arr.users.push(faker.fake("{{name.firstName}} {{name.lastName}}"));
+//   }
+
+//   testData.push(arr);
+// }
+
+export default [
+  {
+    pod: "Account Room",
+    users: [
+      "Alyson Balistreri",
+      "Cleo Konopelski",
+      "Darian Kub",
+      "Moises Wiegand",
+      "Kaylie Russel",
+      "Baylee Harber",
+      "Katelyn Quitzon",
+      "Bruce Steuber",
+      "Harvey Pagac",
+      "Devan Mann",
+    ],
+  },
+  {
+    pod: "HTTP Room",
+    users: [
+      "Rowland Price",
+      "Gracie Boehm",
+      "Elsie Kertzmann",
+      "Catherine Durgan",
+      "Buck Satterfield",
+      "Imani Lockman",
+      "Virgil Corkery",
+      "Claud Kassulke",
+      "Lonnie Schumm",
+      "Jo Farrell",
+    ],
+  },
+  {
+    pod: "Security Room",
+    users: [
+      "Bessie Watsica",
+      "Wilford Schmitt",
+      "Arianna Gerhold",
+      "Nickolas Mitchell",
+      "Theresa West",
+      "Audie Okuneva",
+      "Ricky Schuppe",
+      "Sharon Mayer",
+      "Harley Borer",
+      "Sedrick Dooley",
+    ],
+  },
+  {
+    pod: "Modular Room",
+    users: [
+      "Orie Robel",
+      "Camilla Orn",
+      "Odell Reichert",
+      "Emmie Strosin",
+      "Dejon Schimmel",
+      "Cordie Haag",
+      "Ludie Denesik",
+      "Russel Williamson",
+      "Gustave Kautzer",
+      "Simone Romaguera",
+    ],
+  },
+  {
+    pod: "Grocery Room",
+    users: [
+      "Johnson Dietrich",
+      "Geovany Kiehn",
+      "Blake Barrows",
+      "Raphaelle Torphy",
+      "Joey Lakin",
+      "Adam Toy",
+      "Loraine Okuneva",
+      "Joe Abbott",
+      "Theresa Johnson",
+      "Laury Pacocha",
+    ],
+  },
+  {
+    pod: "Industrial Room",
+    users: [
+      "Golden Altenwerth",
+      "Gustave Balistreri",
+      "Brooks Prohaska",
+      "Shyann MacGyver",
+      "Travon Crist",
+      "Emilia Von",
+      "Susanna Hintz",
+      "River Hudson",
+      "Rosa Von",
+      "Alexandra Hickle",
+    ],
+  },
+  {
+    pod: "Incubate Room",
+    users: [
+      "Laurence Kulas",
+      "Dagmar Mayer",
+      "Allene Collier",
+      "Cali Corwin",
+      "Colt Schoen",
+      "Sharon Howell",
+      "Stan Ruecker",
+      "Shanna Sauer",
+      "Clifton Beier",
+      "Jasmin Green",
+    ],
+  },
+  {
+    pod: "Licensed Room",
+    users: [
+      "Antoinette Kreiger",
+      "Jamey Mosciski",
+      "Brian Herman",
+      "Kristofer Douglas",
+      "Meggie Altenwerth",
+      "Violette Altenwerth",
+      "Conrad Waters",
+      "Lazaro McGlynn",
+      "Edgardo Stoltenberg",
+      "Myrl Donnelly",
+    ],
+  },
+  {
+    pod: "E-markets Room",
+    users: [
+      "Esther Strosin",
+      "Jamal Haag",
+      "Willa Barrows",
+      "Ewell Langosh",
+      "Kellen Murray",
+      "Carlie Stroman",
+      "Shanny Von",
+      "Jessy Kemmer",
+      "Jazlyn Koch",
+      "Samantha Mayert",
+    ],
+  },
+  {
+    pod: "Compressing Room",
+    users: [
+      "Darian Cartwright",
+      "Winifred Weber",
+      "Lennie Bechtelar",
+      "Rosemary Rogahn",
+      "Natalia McClure",
+      "Chester Herman",
+      "Kurtis Dickens",
+      "Halle Ledner",
+      "Martina Kozey",
+      "Eloise Monahan",
+    ],
+  },
+  {
+    pod: "Innovate Room",
+    users: [
+      "Wallace Corwin",
+      "Roel Satterfield",
+      "Milton Runolfsson",
+      "Zoey O'Connell",
+      "Nestor Nicolas",
+      "Arely Roob",
+      "Michele Turner",
+      "Judge Beer",
+      "Omari Parisian",
+      "Adell Kohler",
+    ],
+  },
+  {
+    pod: "Pizza Room",
+    users: [
+      "Kaleb Renner",
+      "Graham Daniel",
+      "Helene Kemmer",
+      "Nathanial Kihn",
+      "Lon Jacobi",
+      "Elise Hamill",
+      "Laverne Kutch",
+      "Porter Hayes",
+      "Braden West",
+      "Kyle Morissette",
+    ],
+  },
+  {
+    pod: "Oro Room",
+    users: [
+      "Elisa Sporer",
+      "Tad Parker",
+      "Mohamed Daniel",
+      "Tracy Rice",
+      "Nicklaus Crona",
+      "Bradford Hilll",
+      "Mustafa Carter",
+      "Henry Erdman",
+      "Arianna Altenwerth",
+      "Earnestine Buckridge",
+    ],
+  },
+  {
+    pod: "Manager Room",
+    users: [
+      "Lon Wisoky",
+      "Jerel Osinski",
+      "Irving Olson",
+      "Barry Shields",
+      "Andres Zieme",
+      "Elinor Parisian",
+      "Lemuel Gottlieb",
+      "Emelie Mante",
+      "Christina Zulauf",
+      "Cordie Berge",
+    ],
+  },
+  {
+    pod: "Redundant Room",
+    users: [
+      "Bernice Morissette",
+      "Malinda Rutherford",
+      "Brent Stroman",
+      "Annabell Schiller",
+      "Golden Altenwerth",
+      "Kitty Hayes",
+      "Alek Trantow",
+      "Amparo Rempel",
+      "Shyann Hudson",
+      "Fabian Swift",
+    ],
+  },
+];
